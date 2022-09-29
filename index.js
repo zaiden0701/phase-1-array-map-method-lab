fa8d0e74b1718416c25d5f69e72fba6f3c3ac07d
Aiden Zafar <+>
2022-09-29 14:42:29 -0400

    commit tree 16bf4587e17587974c9ad1f0ace3d69ee997114d

--- index.js
+++ index.js
@@ -9,8 +9,40 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
+
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// const titleCased = tutorials.map(tutorial => {
+//   const words = tutorial.split(" ").map((word =>
+//   word[0].toUpperCase() + word.slice(1))); 
+//   return words
+// })
+
+// const titleCased = () => {
+//   tutorials.map(function(tutorials){
+//     tutorials = tutorials.split(" ")
+//     for(let i = 0; i < tutorials.length; i++){
+//       console.log(tutorials[i] = tutorials[i].charAt(0).toUpperCase + tutorials[i].slice(1))
+//     }
+//     tutorials.join(" ")
+//   })
+//   return tutorials
+// }
+
+// const titleCased = tutorials.map(tutorial => {
+//   tutorial.split(" ")
+//   tutorial.map((words) => words[0].toUpperCase())
+//   tutorial.join(" ")
+//   return tutorial
+// })
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    let eachWord = tutorial.split(" ")
+    let sentences = eachWord.map((words) => words[0].toUpperCase() + words.slice(1))
+    return sentences.join(" ")
+  }
+)}
